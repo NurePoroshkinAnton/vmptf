ba89a3c361c237a923b37c73c3cbd949369dd175
Anton Poroshkin <anton.poroshkin@nure.ua>
2024-04-27 18:57:09 +0300

    chore: setup frontend app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import { Typography, App as AntdApp } from "antd"
+
+export default function App() {
+    return (
+        <AntdApp>
+            <Typography.Title>UniTube</Typography.Title>
+        </AntdApp>
+    )
+}
+
